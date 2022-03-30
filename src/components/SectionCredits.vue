e7e03554f3f22914e07fe1369a587124566cc265
dragonjet <jetriconew@gmail.com>
2022-03-30 04:18:45 -0700

    Credits: header artwork

--- src/components/SectionCredits.vue
+++ src/components/SectionCredits.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-row no-gutters class="mb-4">
       <v-col cols="12" class="text-center black--text">
-        For Reimu Endou. From Phantomos.
+        For Reimu Endou. From Phantomos. Header Artwork by @_YuRaKi_.
       </v-col>
     </v-row>
   </v-container>
