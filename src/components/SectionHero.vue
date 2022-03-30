2bcb59940b80d298738f106df9ef454bee2e2e11
dragonjet <jetriconew@gmail.com>
2022-03-30 03:33:20 -0700

    Design update

--- src/components/SectionHero.vue
+++ src/components/SectionHero.vue
@@ -1,19 +1,20 @@
 <template>
   <v-container class="section-grid section-box pb-8 mb-16">
-    <v-row no-gutters class="mb-4">
+    <!-- <v-row no-gutters class="mb-4">
       <v-col cols="12">
         <h1 class="yellow--text text--darken-3 text-center pa-0 mx-0 my-0">
           <img
             src="../assets/img/section-happy.png" height="120"
-            alt="Happy 6-Months and Congrats on 200K Millie!" />
+            alt="Happy 6-Months Anniversary Reimu!" />
         </h1>
       </v-col>
-    </v-row>
-    <v-row no-gutters>
+    </v-row> -->
+    <v-row no-gutters class="pt-8">
       <v-col cols="6"
-        class="text-right white--text text-h5 text--darken-1 pt-4 pr-8 pl-4 hero-text">
+        class="text-right black--text text-h5
+          text--darken-1 pt-4 pr-8 pl-4 font-weight-bold hero-text">
         <p>
-
+          message from phantomos
         </p>
       </v-col>
       <v-col cols="6" class="pr-8">
@@ -40,7 +41,7 @@ export default {
 
 <style lang="scss" scoped>
 .section-grid {
-  margin-top:300px;
+  margin-top:400px;
   max-width:1280px;
   .hero-text {
     white-space: pre-line;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="page-home" class="pt-2 pb-4">
+  <div id="page-home" class="pb-4">
     <div class="header"></div>
     <div class="section mb-4">
       <SectionHero/>
@@ -14,9 +14,6 @@
       <SectionMessages/>
     </div>
     <div class="section mb-12">
-      <SectionGallery/>
-    </div>
-    <div class="section mb-12">
       <SectionCredits/>
     </div>
   </div>
@@ -27,7 +24,6 @@ import SectionHero from '@/components/SectionHero.vue';
 import SectionImage from '@/components/SectionImage.vue';
 import SectionTimeline from '@/components/SectionTimeline.vue';
 import SectionMessages from '@/components/SectionMessages.vue';
-import SectionGallery from '@/components/SectionGallery.vue';
 import SectionCredits from '@/components/SectionCredits.vue';
 
 export default {
@@ -37,7 +33,6 @@ export default {
     SectionImage,
     SectionTimeline,
     SectionMessages,
-    SectionGallery,
     SectionCredits,
   },
 };
@@ -45,25 +40,29 @@ export default {
 
 <style lang="scss" scoped>
 #page-home {
-  background-color:#505050;
+  background-color:#fcfcfc;
   background-image:url('../assets/img/background.png');
   background-repeat: repeat;
   min-height:100vh;
+  position:relative;
+  padding-top:1px;
 }
 .header {
   width:100%;
-  height:300px;
-  // background-image:url('../assets/img/banner.png');
-  background-position:50%;
+  height:1000px;
+  background-image:url('../assets/img/banner.png');
+  background-position:50% -30px;
   background-repeat:no-repeat;
+  background-size:1160px;
   position:absolute;
+  top:0;
 }
 .section {
   position:relative;
   .section-grid {
     min-height:400px;
     &.section-box {
-      background-color:rgb(200, 200, 200, 0.75);
+      background-color:rgb(235, 235, 235, 0.85);
       border-color:#B90B4A;
       border-right:4px solid #B90B4A;
       border-bottom:4px solid #B90B4A;
