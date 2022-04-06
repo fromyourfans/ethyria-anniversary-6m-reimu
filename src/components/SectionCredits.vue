c1951f39bab3f11c6f5f0d6ade4b6d21237c1b88
dragonjet <jetriconew@gmail.com>
2022-04-05 23:50:27 -0700

    Updated credits

--- src/components/SectionCredits.vue
+++ src/components/SectionCredits.vue
@@ -2,7 +2,16 @@
   <v-container>
     <v-row no-gutters class="mb-4">
       <v-col cols="12" class="text-center black--text">
-        For Reimu Endou. From Phantomos. Header Artwork by @_YuRaKi_.
+        <p>For Reimu Endou. From Phantomos.</p>
+        <p>
+          Management: Reimucord Staff<br />
+          Programming: jetrico<br />
+          Header Artwork: @_YuRaKi_<br />
+          Reimu assets (icons, sprites): @Nnnoira<br />
+          Feature Video: Enna's left feather<br />
+          Drawing Board: ____<br />
+          and all phantomos who celebrate these milestones with Reimu and Ethyria!
+        </p>
       </v-col>
     </v-row>
   </v-container>
